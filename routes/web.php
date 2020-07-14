809a47a4f06db87c21ee3e15d24082ec9b52dd8d
robin <robin.sgeorges@gmail.com>
2020-07-14 13:41:17 +0200

    added CORS middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\CoursController as CC;
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 
