f86c04a2840871aa96afe1fd7399863e2f0778ab
robin <robin.sgeorges@gmail.com>
2020-07-14 23:57:24 +0200

    resolving CORS error

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -20,7 +20,8 @@ class CorsMiddleware
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With',
+            'Content-Type' => 'application/x-www-form-urlencoded'
         ];
 
         if ($request->isMethod('OPTIONS'))
