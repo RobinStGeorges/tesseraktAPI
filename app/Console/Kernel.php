11ff0a8c97c3c8c4114d07719f26829f61a2e6a1
robin <robin.sgeorges@gmail.com>
2020-07-14 15:34:34 +0200

    added CORS middleware

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
